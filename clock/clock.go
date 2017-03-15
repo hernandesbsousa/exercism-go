8037216f861890bc105ee0f63e7587886e59a927
Hernandes B. de Sousa <hernandes.sousa@gmail.com>
2017-03-14 23:16:32 -0300

    First solution try for Clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,26 +1,61 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
 package clock
 
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
+import "strconv"
+
 const testVersion = 4
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
-
-type Clock // Complete the type definition.  Pick a suitable data type.
+type Clock struct {
+	hour, minute int
+}
 
 func New(hour, minute int) Clock {
+	return validate(hour, minute)
 }
 
-func (Clock) String() string {
+func validate(hour, minute int) Clock {
+	for hour > 24 || minute > 59 {
+		if hour > 24 {
+			hour -= 24
+		}
+		if minute > 59 {
+			minute -= 60
+			hour++
+		}
+	}
+
+	for hour < 0 || minute < 0 {
+		if hour < 0 {
+			hour += 24
+		}
+		if minute < 0 {
+			minute += 60
+			hour--
+		}
+	}
+
+	if hour == 24 {
+		hour = 0
+	}
+	return Clock{hour, minute}
 }
 
-func (Clock) Add(minutes int) Clock {
+func (clock Clock) String() string {
+	var hour, minute string
+
+	if clock.hour < 10 {
+		hour = "0" + strconv.Itoa(clock.hour)
+	} else {
+		hour = strconv.Itoa(clock.hour)
+	}
+	if clock.minute < 10 {
+		minute = "0" + strconv.Itoa(clock.minute)
+	} else {
+		minute = strconv.Itoa(clock.minute)
+	}
+	return hour + ":" + minute
 }
 
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
+func (clock Clock) Add(minutesToAdd int) Clock {
+	clock.minute += minutesToAdd
+	return validate(clock.hour, clock.minute)
+}
